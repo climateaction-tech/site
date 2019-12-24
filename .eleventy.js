2d421af02d4a794b6a5a13e1d6c3c53939203f9b
Asim Hussain <asim@codecraft.tv>
2019-12-24 02:29:15 +0000

    Updated navigation and centralised assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,5 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("img")
-    eleventyConfig.addPassthroughCopy("css")
-    eleventyConfig.addPassthroughCopy("js")
-    eleventyConfig.addPassthroughCopy("vendor")
+    eleventyConfig.addPassthroughCopy("assets")
     return {
         markdownTemplateEngine: "liquid",
         dir: {
